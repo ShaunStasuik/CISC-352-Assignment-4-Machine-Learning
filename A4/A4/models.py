f26443c54a01bd193316513396a8e89d6c825a88
sstasuik <shaunstasuik@gmail.com>
2026-03-28 17:22:19 -0400

    Q3 Implemented: Test Accuracy 97.3%

--- A4/A4/models.py
+++ A4/A4/models.py
@@ -97,8 +97,26 @@ class DigitClassificationModel(object):
     """
     def __init__(self):
         # Initialize your model parameters here
-        "*** YOUR CODE HERE ***"
+        # Hyperparameters
+        self.batch_size = 100
+        self.learning_rate = 0.1
 
+        # 784 -> 200 -> 100 -> 10
+        self.w1 = nn.Parameter(784, 200)
+        self.b1 = nn.Parameter(1, 200)
+
+        self.w2 = nn.Parameter(200, 100)
+        self.b2 = nn.Parameter(1, 100)
+
+        self.w3 = nn.Parameter(100, 10)
+        self.b3 = nn.Parameter(1, 10)
+
+        self.parameters = [
+            self.w1, self.b1,
+            self.w2, self.b2,
+            self.w3, self.b3
+        ]
+        
     def run(self, x):
         """
         Runs the model for a batch of examples.
@@ -113,7 +131,10 @@ class DigitClassificationModel(object):
             A node with shape (batch_size x 10) containing predicted scores
                 (also called logits)
         """
-        "*** YOUR CODE HERE ***"
+        h1 = nn.ReLU(nn.AddBias(self.b1, nn.Linear(x, self.w1)))
+        h2 = nn.ReLU(nn.AddBias(self.b2, nn.Linear(h1, self.w2)))
+        logits = nn.AddBias(self.b3, nn.Linear(h2, self.w3))
+        return logits
 
     def get_loss(self, x, y):
         """
@@ -128,11 +149,23 @@ class DigitClassificationModel(object):
             y: a node with shape (batch_size x 10)
         Returns: a loss node
         """
-        "*** YOUR CODE HERE ***"
+        return nn.SoftmaxLoss(self.run(x), y)
 
     def train_model(self, data):
         """
         Trains the model.
         """
-        "*** YOUR CODE HERE ***"
+        target_validation_accuracy = 0.975
+        max_epochs = 20
+
+        for _ in range(max_epochs):
+            for x, y in data.iterate_once(self.batch_size):
+                loss = self.get_loss(x, y)
+                gradients = nn.gradients(self.parameters, loss)
+
+                for parameter, gradient in zip(self.parameters, gradients):
+                    parameter.update(-self.learning_rate, gradient)
+
+            if data.get_validation_accuracy() >= target_validation_accuracy:
+                break
 
